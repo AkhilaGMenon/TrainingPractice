8c79efcd8bd62538789c283b853dc04f86063914
U94362 <u94362@ust-global.com>
2020-11-28 18:43:22 +0530

    first commit

---
+++ src/main/java/com/ust/practice/model/Message.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name  :test
+ * 
+ */
+package com.ust.practice.model;
+
+import lombok.Data;
+
+/**
+ * Model class for Message consist field message with getters and setters methods
+ * 
+ * @author Akhila
+ *
+ */
+@Data
+public class Message {
+	public String message;
+
+	
+	
+
+}
